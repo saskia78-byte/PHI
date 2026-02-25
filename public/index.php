5f5a4eb7314f6804f295d44ca7fb5df75c9fd831
hiskala <hiskalab@gmail.com>
2026-02-25 14:45:39 +0100

    Ajout Core + Controllers + Views + update index

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    use App\Core\Router;
+
+    $router = new Router();
+    $controller = $_GET['controller'] ?? 'pages';
+    $action = $_GET['action'] ?? 'home';
+
+    $router->callController($controller, $action);
